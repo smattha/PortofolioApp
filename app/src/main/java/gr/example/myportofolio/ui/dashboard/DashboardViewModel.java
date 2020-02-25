36daa98e988d1732689430beb4d1f6dc9eb4be57
Matthaiakis Stergios <stergiosmat@gmail.com>
2020-02-25 05:31:05 +0200

    -- this is the initial commit

---
+++ app/src/main/java/gr/example/myportofolio/ui/dashboard/DashboardViewModel.java
@@ -0,0 +1,19 @@
+package gr.example.myportofolio.ui.dashboard;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class DashboardViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public DashboardViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is dashboard fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
